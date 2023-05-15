9a9c8be58f55aed790977e8ba86da89d05ebc0cf
Prakhar <apple@Prakhars-MacBook-Air.local>
2023-05-15 08:17:59 +0530

    setup done

---
+++ build/lib/src/load_data.py
@@ -0,0 +1,20 @@
+# read data from data scource
+# save it in the data/raw for further process
+
+import os
+from get_data import read_params, get_data
+import argparse
+
+def loadandsave(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    new_cols = [cols.replace(" ","_") for cols in df.columns]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    df.to_csv(raw_data_path,sep=",",header=new_cols,index=False)
+    print(new_cols)
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    data = loadandsave(config_path=parsed_args.config)
