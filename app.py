87fef6ecfc75a218775c5ae631681e253cf4611e
Prakhar <apple@Prakhars-MacBook-Air.local>
2023-05-29 07:43:44 +0530

    github action workflow added

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import yaml
+import joblib
+import numpy as np
+
+params_path = "params.yaml"
+webapp_root = "webapp"
+
+static_dir = os.path.join(webapp_root,"static")
+template_dir = os.path.join(webapp_root,"templates")
+
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
+
+def read_params(config_path=params_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data).tolist()[0]
+    print(prediction)
+    return prediction
+
+
+def api_response(dict_request):
+    try:
+        if validate_input(dict_request):
+            data = np.array([list(dict_request.values())])
+            response = predict(data)
+            response = {"response": response}
+            return response
+
+    # except NotInRange as e:
+    #     response = {"the_exected_range": get_schema(), "response": str(e)}
+    #     return response
+    #
+    # except NotInCols as e:
+    #     response = {"the_exected_cols": get_schema().keys(), "response": str(e)}
+    #     return response
+    except Exception as e:
+        response = {"response": str(e)}
+        return response
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float,data))]
+                response = predict(data)
+                return render_template("index.html", response = response)
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+
+        except Exception as e:
+            print(e)
+            error = "Something went wrong! Try again"
+            # return rendet_template("404.html", error=error)
+            return error
+    else:
+        return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
